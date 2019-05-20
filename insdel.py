071625f16eb44167ff4ab7c7db38925c5366efe5
mleijon <mikael.leijon@gmail.com>
2019-05-21 00:40:40 +0200

    :-)

--- insdel.py
+++ insdel.py
@@ -17,13 +17,15 @@ def mk_mod_dict(name):
         sys.exit('Modification file not found')
     mod_dict = dict()
     with open(ARGS.m) as infile:
-        line = infile.readline()
-        mod = line.strip().split(',')
-        if mod[0] in mod_dict:
-            mod_dict[mod[0]].append((mod[1], mod[2]))
-        else:
-            mod_dict[mod[0]] = (mod[1], mod[2])
-        print(mod_dict)
+        lines = infile.readlines()
+        for line in lines:
+            mod = line.strip().split(',')
+            if mod[0] in mod_dict:
+                mod_dict[mod[0]].append((mod[1], mod[2]))
+            else:
+                mod_dict[mod[0]] = [(mod[1], mod[2])]
+    infile.close()
+    return mod_dict
 
 
 PARSER = argparse.ArgumentParser(description='String modification file')
@@ -31,14 +33,19 @@ PARSER.add_argument('-d', type=str, help='Input directory', required=True)
 PARSER.add_argument('-m', type=str, help='modification file', default='mod.txt')
 ARGS = PARSER.parse_args()
 
+modifications = mk_mod_dict(ARGS.m)
 try:
     with os.scandir(ARGS.d) as it:
         for entry in it:
             if not entry.name.startswith('.') and entry.is_file():
-                print(entry.name)
+                tmpfile = open('modfile.tmp','w')
+                with open(it) as infile:
+                    all_lines = infile.readlines()
+
 except FileNotFoundError:
     sys.exit("'{}' not found".format(ARGS.d))
 except NotADirectoryError:
     sys.exit("'{}' is not a directory".format(ARGS.d))
-mk_mod_dict(ARGS.m)
+
+
 
