a3f6ab4fa92ab0f244c1ec1120ddbd35002ef369
mleijon <36968756+mleijon@users.noreply.github.com>
2021-01-12 15:33:37 +0100

    :-)

--- exactFisher_and_chi2.py
+++ exactFisher_and_chi2.py
@@ -2,7 +2,7 @@
 
 import scipy.stats as stats
 
-oddsratio, pvalue = stats.fisher_exact([[7, 74], [1, 77]])
+oddsratio, pvalue = stats.fisher_exact([[1, 80], [4, 74]])
 print(pvalue)
 print('****')
 ch2stat, ch2p = stats.chisquare([20, 22, 27, 7], f_exp=[15.1026, 28.7436, 23.3846, 8.7692], ddof=2)
